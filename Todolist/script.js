070d6de660935026e3914560abfd29a197ce5d99
yungstarry <111007711+yungstarry@users.noreply.github.com>
2023-03-03 12:29:45 +0100

    todolist

--- Todolist/script.js
+++ Todolist/script.js
@@ -1,55 +1,50 @@
-const form = document.getElementById('task-form');
-const taskInput = document.getElementById('task-input');
-const taskList = document.getElementById('task-list');
+// Select HTML elements
+const taskForm = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task-input');
+const taskList = document.querySelector('#task-list');
+
 // Load tasks from local storage
-const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-// Add tasks to the list
-function addTask(task) {
-	// Create the task HTML element
-	const li = document.createElement('li');
-	li.innerHTML = `${task}<button class="delete-btn">Delete</button>`;
-	taskList.appendChild(li);
+// Display tasks in the list
+function displayTasks() {
+  taskList.innerHTML = '';
+  tasks.forEach((task, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <span class="task">${task}</span>
+      <button class="delete-btn" data-index="${index}">Delete</button>
+    `;
+    taskList.appendChild(li);
+  });
+}
 
-	// Save the task in local storage
-	tasks.push(task);
-	localStorage.setItem('tasks', JSON.stringify(tasks));
+// Add new task to the list
+function addTask(event) {
+  event.preventDefault();
+  const taskText = taskInput.value.trim();
+  if (taskText) {
+    tasks.push(taskText);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    displayTasks();
+    taskInput.value = '';
+    taskInput.focus();
+  }
 }
 
 // Remove task from the list
-function removeTask(taskIndex) {
-	// Remove the task HTML element
-	const li = taskList.getElementsByTagName('li')[taskIndex];
-	li.remove();
-
-	// Remove the task from the local storage
-	tasks.splice(taskIndex, 1);
-	localStorage.setItem('tasks', JSON.stringify(tasks));
+function removeTask(event) {
+  if (event.target.classList.contains('delete-btn')) {
+    const index = event.target.dataset.index;
+    tasks.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    displayTasks();
+  }
 }
 
-// Load tasks from local storage on page load
-function loadTasks() {
-	tasks.forEach(task => addTask(task));
-}
+// Add event listeners
+taskForm.addEventListener('submit', addTask);
+taskList.addEventListener('click', removeTask);
 
-// Add task on form submit
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	const task = taskInput.value.trim();
-	if (task !== '') {
-		addTask(task);
-		taskInput.value = '';
-		taskInput.focus();
-	}
-});
-
-// Remove task on delete button click
-taskList.addEventListener('click', e => {
-	if (e.target.classList.contains('delete-btn')) {
-		const taskIndex = Array.from(e.target.parentNode.parentNode.children).indexOf(e.target.parentNode);
-		removeTask(taskIndex);
-	}
-});
-
-// Load tasks from local storage on page load
-loadTasks();
+// Load tasks on page load
+displayTasks();
